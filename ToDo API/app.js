2fb41e4de09be2e9daf1e4248bc0fb3c95d70b23
Andreas Schneider <hnsdieter@live.de>
2020-10-14 18:51:11 +0200

    finished app

--- ToDo API/app.js
+++ ToDo API/app.js
@@ -3,22 +3,33 @@ const app = express();
 
 const PORT = 3000;
 const todos = [{
+        id: 1,
         title: 'ToDo 1',
         checked: false,
         date: new Date(),
     },
     {
+        id: 2,
         title: 'ToDo 2',
         checked: true,
         date: new Date(),
     }
 ];
 
+app.use(express.json());
 app.use(express.static('public'));
 
 // CREATE
 app.put('/todo', function(req, res) {
-    res.send('Hallo Welt!')
+    const body = req.body;
+    const new_todo = {
+        id: todos.length + 1,
+        title: body.title,
+        checked: false,
+        date: new Date(),
+    }
+    todos.push(new_todo);
+    res.send(new_todo);
 });
 
 // READ
@@ -27,13 +38,22 @@ app.get('/todo', function(req, res) {
 });
 
 // UPDATE
-app.post('/todo', function(req, res) {
-    res.send('Hallo Welt!')
+app.post('/todo/:id', function(req, res) {
+    const id = req.params.id;
+
+    const todo = todos.find(elem => elem.id == id);
+    todo.checked = !todo.checked;
+
+    res.send({ message: 'success', todo: todo });
 });
 
 // DELETE
-app.delete('/todo', function(req, res) {
-    res.send('Hallo Welt!')
+app.delete('/todo/:id', function(req, res) {
+    const id = req.params.id;
+
+    const index = todos.findIndex(elem => elem.id == id);
+    todos.splice(index, 1);
+    res.send({ message: 'success' })
 });
 
 app.listen(PORT, function() {
--- ToDo API/public/main.js
+++ ToDo API/public/main.js
@@ -1,10 +1,99 @@
-fetch('/todo')
-    .then(function(response) {
+function create(title) {
+    const data = {
+        title: title,
+    }
+    fetch('/todo', {
+        method: 'PUT',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }).then(function(response) {
         return response.json();
-    }).then(function(todos) {
-        const list = document.querySelector('#todos');
-        for (const todo of todos) {
-            const item = `<li>${todo.title}</li>`;
-            list.insertAdjacentHTML('beforeend', item);
-        }
+    }).then(function(json) {
+        addToList(json);
     });
+}
+
+function update(id) {
+    fetch(`/todo/${id}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}),
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const listitem = document.querySelector(`li[data-todo="${id}"]`);
+        listitem.classList.toggle('checked');
+    });
+}
+
+function del(id) {
+    fetch(`/todo/${id}`, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}),
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        if (json.message !== 'success') {
+            return;
+        }
+        const listitem = document.querySelector(`li[data-todo="${id}"]`);
+        listitem.remove();
+    });
+}
+
+function addToList(todo) {
+    const list = document.querySelector('#todos');
+    const item = `<li data-todo="${todo.id}" class="todo ${todo.checked ? 'checked' : ''}">
+        <input type="checkbox" class='check' data-todo="${todo.id}" ${todo.checked ? 'checked' : ''}>
+        ${todo.title}
+        <button data-todo="${todo.id}">Löschen</button>
+    </li>`;
+    list.insertAdjacentHTML('beforeend', item);
+
+    const box = document.querySelector(`input[data-todo="${todo.id}"]`);
+    box.addEventListener('change', function(event) {
+        const id = event.target.dataset.todo;
+        update(id);
+    });
+
+    const deleteBtn = document.querySelector(`button[data-todo="${todo.id}"]`);
+    deleteBtn.addEventListener('click', function(event) {
+        const id = event.target.dataset.todo;
+        del(id);
+    });
+}
+
+function initView() {
+    const form = document.querySelector('#new-todo');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const title = event.target.title.value;
+
+        event.target.title.value = '';
+        create(title);
+    });
+
+    fetch('/todo')
+        .then(function(response) {
+            return response.json();
+        }).then(function(todos) {
+            for (const todo of todos) {
+                addToList(todo);
+            }
+        });
+}
+
+window.onload = function() {
+    initView();
+}
