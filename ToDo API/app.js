137a794f1a560d057f1eb48fa2167e5907836c9b
Andreas Schneider <hnsdieter@live.de>
2020-10-15 11:56:45 +0200

    finished create

--- ToDo API/app.js
+++ ToDo API/app.js
@@ -14,6 +14,7 @@ const todos = [{
     }
 ];
 
+app.use(express.json());
 app.use(express.static('public'));
 
 // CREATE
--- ToDo API/public/main.js
+++ ToDo API/public/main.js
@@ -4,6 +4,24 @@ function showTodo(todo) {
     list.insertAdjacentHTML('beforeend', item);
 }
 
+function create(title) {
+    const data = {
+        title: title,
+    };
+
+    fetch('/todo', {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(function(response) {
+        return response.json();
+    }).then(function(todo) {
+        showTodo(todo);
+    });
+}
+
 function showAll() {
     fetch('/todo')
         .then(function(response) {
@@ -23,7 +41,11 @@ function initView() {
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
-        console.log('HI');
+        const target = event.target;
+        const title = target.title.value;
+        target.title.value = '';
+
+        create(title);
     });
 }
 
