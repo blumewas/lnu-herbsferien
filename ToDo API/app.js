2b566bc19afbba2720b9ae99c47db4358b39392c
Andreas Schneider <hnsdieter@live.de>
2020-10-16 15:06:34 +0200

    fertigstellung der Kommentare

--- ToDo API/app.js
+++ ToDo API/app.js
@@ -1,77 +1,112 @@
-const fs = require('fs');
-const express = require('express');
+// importieren der benötigten Bibliotheken
+const fs = require('fs'); // fs ist eine Bibliothek um Dateien zu lesen und zu schreiben
+const express = require('express'); // express.js ist eine Bibliothek um REST-Apis zu erstellen.
+// Initialisieren einer express-App
 const app = express();
 
 const PORT = 3000;
+// Array in dem unsere Todos gespeichert werden.
 const todos = [];
 
+// einstellen des Body-Parsers, so dass express versteht, was wir dan den body senden
 app.use(express.json());
+// verwendung der APP als Webserver, über localhost:PORT im Browser wird die index.html angezeigt.
 app.use(express.static('public'));
 
 // CREATE
+// erstellen einer API-Methode mit der Funktion "PUT"
+// der zweite Parameter ist eine Funktion, die die Anfrage und die Antwort
+// an den anfragenden Client bearbeitet
 app.put('/todo', function(req, res) {
+    // Auslesen des body der Anfrage (Daten die der Client mit der Anfrage schickt)
     const body = req.body;
-
+    // Wert der Variable title aus dem body auslesen
+    // dieser muss von client mitgesendet werden
     const title = body.title;
-
+    // Erstellen eines neuen Todos mit der Methode createTodo
     const newTodo = createTodo(title);
-    saveTodos();
+    saveTodos(); // Speichern der Todos
 
-    res.send(newTodo);
+    res.send(newTodo); // Senden des erstellten Todo an den Client
 });
 
 // READ
+// API METHODE GET, zum auslesen aller todos
 app.get('/todo', function(req, res) {
+    // Senden aller Todos an den Client
     res.send(todos);
 });
 
 // UPDATE
+// erstellen einer API-Methode mit der Funktion "POST"
+// der zweite Parameter ist eine Funktion, die die Anfrage und die Antwort
+// an den anfragenden Client bearbeitet
 app.post('/todo/:id', function(req, res) {
+    // auslesen des URL-Parameters id
     const id = req.params.id;
 
+    // Suche des angefragten Todo Elements
     const todo = todos.find(elem => id === elem.id);
+    // Ändern des Status unseres Todos
     todo.checked = !todo.checked;
 
-    saveTodos();
-    res.send(todo);
+    saveTodos(); // Speichern der Todos
+    res.send(todo); // Antwort an Client senden mit dem bearbeiteten TODO
 });
 
 // DELETE
-app.delete('/todo', function(req, res) {
-    res.send('Hallo Welt!')
+// erstellen einer API-Methode mit der Funktion "DELETE"
+// der zweite Parameter ist eine Funktion, die die Anfrage und die Antwort
+// an den anfragenden Client bearbeitet
+app.delete('/todo/:id', function(req, res) {
+    // auslesen des URL-Parameters id
+    const id = req.params.id;
+    // suchen des index des zu löschenden Todos
+    const index = todos.findIndex(elem => elem.id == id);
+    todos.splice(index, 1); // entfernen eines Elements aus dem Array ab dem gefundenen index (das zulöschende Todo)
+    res.send({ message: 'success' }); // Bestättigende Nachricht als Antwort senden
 });
 
+// Starten unserer App/API
 app.listen(PORT, function() {
-    loadTodos();
+    loadTodos(); // Aufruf der Funktion zum laden unserer Todos
     console.log(`App wurde gestartet und läuft auf http://localhost:${PORT}`);
 });
-
+// Funktion zur Erstellung eines Todos
+// einziges Argument ist der Titel, alle anderen Infos sind mehr oder minder vorbestimmt
 function createTodo(title) {
+    // neues Todo Objekt inititaliseren
     const newTodo = {
-        id: todos.length + 1,
-        title: title,
-        checked: false,
-        date: new Date()
-    }
+            id: todos.length + 1, // auto-increment der id mit Hilfe der Anzahl der aktuell geladenen Todos
+            title: title,
+            checked: false,
+            date: new Date() // erstellen des Timestamps, an dem das Todo erstellt wird
+        }
+        // Hinzufügen des Todos in unser Array
     todos.push(newTodo);
+    // Rückgabe des erstellten Todos um es weiterzu verarbeiten
     return newTodo;
 }
-
+// Speichern der Todos in der Datei todos.json
 function saveTodos() {
+    // umwandeln unseres Todo-Arrays in einen String
     const todosStr = JSON.stringify(todos);
-
+    // überschreiben des Inhaltes der Datei todos.json mit unserem String
     fs.writeFileSync('./todos.json', todosStr);
     console.log('Saved all Todos successfully!');
 }
 
+// Laden der Todos aus der Datei todos.json
 function loadTodos() {
     const content = fs.readFileSync('./todos.json');
-
+    // content ist ein Objekt vom Typ Buffer
+    // toString() liefert uns den Inhalt im Text-Format
+    // JSON.parse macht aus einem String/Text ein JS-Objekt
     const loadedTodos = JSON.parse(content.toString());
-
+    // Hinzufügen unserer geladenen Todos in unser Todos-Array
     for (const loaded of loadedTodos) {
         todos.push(loaded);
     }
-
+    // Ausgabe Anzahl der geladenen Todos
     console.log(`Loaded ${todos.length} Todos!`);
 }
--- ToDo API/public/main.js
+++ ToDo API/public/main.js
@@ -1,52 +1,113 @@
+// Method um ein Todo zu unserer Liste hinzuzufügen
 function showTodo(todo) {
     const list = document.querySelector('#todos');
-    const item = `<li>${todo.title}</li>`;
-    list.insertAdjacentHTML('beforeend', item);
+    const item = `<li data-todo="${todo.id}" class="todo ${todo.checked ? 'checked' : ''}">
+        <input type="checkbox" class='check' data-todo="${todo.id}" ${todo.checked ? 'checked' : ''}>
+        ${todo.title}
+        <button data-todo="${todo.id}">Löschen</button>
+    </li>`;
+    list.insertAdjacentHTML('beforeend', item); // honzufügen des Elementes zur Website
+
+    // Hinzufügen eines Eventlisteners, wenn eine checkbox ausgewählt wird
+    const box = document.querySelector(`input[data-todo="${todo.id}"]`);
+    box.addEventListener('change', function(event) {
+        const id = event.target.dataset.todo; // Auslesen des wertes der id, der Wert wird in data-todo gespeichert
+        update(id); // Aufruf der Methode um den Status des Todos zu togglen
+    });
+
+    // Auswahl des delete Buttons, des die Methode zum löschen des servers aufruft
+    const deleteBtn = document.querySelector(`button[data-todo="${todo.id}"]`);
+    deleteBtn.addEventListener('click', function(event) {
+        const id = event.target.dataset.todo; // Auslesen des wertes der id, der Wert wird in data-todo gespeichert
+        del(id); // Aufurf der Methode zum löschen
+    });
 }
 
+// Erstellen eines neuen Todos
 function create(title) {
     const data = {
         title: title,
     };
-
+    // Anfrage an den Server erstellen
     fetch('/todo', {
         method: 'PUT',
-        body: JSON.stringify(data),
+        body: JSON.stringify(data), // der Body der Anfrage, der von der API/dem Server bearbeitet werden sollte
         headers: {
             'Content-Type': 'application/json'
         },
     }).then(function(response) {
         return response.json();
     }).then(function(todo) {
-        showTodo(todo);
+        showTodo(todo); // Hinzufügen des todos auf die Seite
     });
 }
 
+function update(id) {
+    fetch(`/todo/${id}`, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}),
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const listitem = document.querySelector(`li[data-todo="${id}"]`); // Auswahl des Listeneintrags
+        listitem.classList.toggle('checked'); // hinzufügen des Klasse zu einem listitem um das Todo zu markieren, wenn das Häkchen gesetzt ist.
+    });
+}
+
+function del(id) {
+    fetch(`/todo/${id}`, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({}),
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        // Abbruch, sollte die Nachricht nicht success sein, die der Server an uns schickt
+        if (json.message !== 'success') {
+            return;
+        }
+        const listitem = document.querySelector(`li[data-todo="${id}"]`); // Auswahl des Listeneintrags
+        listitem.remove(); // Löschen des Elements aus der Liste
+    });
+}
+
+// Aufruf aller todos
 function showAll() {
+    // Anfrage an den Server
     fetch('/todo')
         .then(function(response) {
-            return response.json();
+            return response.json(); // Die Antwort des Servers auf die Anfrage
         }).then(function(todos) {
+            // for-Schleife für jedes geladene Todo
             for (const todo of todos) {
                 showTodo(todo);
             }
         });
 }
-
+// Methode zum Initialisieren der Website
 function initView() {
     showAll();
 
     const form = document.querySelector('#createForm');
     // const form = document.getElementById('createForm');
+    // Hinzufügen eines Event-Listeners, zu dem Formular
+    // das Event wird ausgelöst, wenn der OK-Button gedrückt wird oder Enter gedrückt wird, wenn der Cursor im Input-Element ist
     form.addEventListener('submit', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // verhindern des neu ladens der Website wenn das Formular abgeschickt wird
 
-        const target = event.target;
-        const title = target.title.value;
-        target.title.value = '';
+        const target = event.target; // auswählen des Formulars um Infos auszulesen
+        const title = target.title.value; // Auslesen des Titels
+        target.title.value = ''; // clearen des Inputs
 
-        create(title);
+        create(title); // Aufruf der Methode zum erstellen des Todos
     });
 }
-
+// Aufruf der Methode initView, wenn die Seite fertig geladen ist
 window.onload = initView;
